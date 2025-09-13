9a604938e7dcd8cdec3f16786c2474802935caca
Bassel-11 <albassel.abobakr11@gmail.com>
2025-09-13 14:24:48 +0300

    file system

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello from node")
+const fs = require("fs");
+
+// Blocking, synchronous way
+
+// fs.writeFileSync("message.txt", "Hello from Node.js 12345 Created on ");
+
+// const data = fs.readFileSync("message.txt", "utf-8");
+// console.log(data);
+
+// Non-blocking, asynchronous way
+
+fs.readFile("./txt/message.txt", "utf-8", (err, data1) => {
+    if (err) return console.log("ERROR 💥");
+        console.log(data1);
+});
+fs.writeFile("./txt/message2.txt", "Nice to meet you", (err) => {
+    if (err) return console.log("ERROR 💥");
+    console.log("File written successfully");
+});
+console.log("Will read file!");
