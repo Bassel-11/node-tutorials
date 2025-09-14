0f2dee75e25fbe3f3ce3cc36e9371ccb791639f6
Bassel-11 <albassel.abobakr11@gmail.com>
2025-09-14 09:52:32 +0300

    simple web server v1

--- index.js
+++ index.js
@@ -28,10 +28,9 @@ const http = require("http");
 
 
 const server = http.createServer((req, res) => {
-    console 
     res.end("Hello from the server!");
 });
 
 server.listen(8000, "127.0.0.1", () => {
     console.log("Server is listening on port 8000");
-});
+}); 
